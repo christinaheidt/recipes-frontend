f4d273e30c63c84383a5a00dacc5a3d3eab61e24
Christina Heidt <heidt.christina@gmail.com>
2020-08-16 09:33:44 +0200

    start packages

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import styled from "styled-components";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Recipes</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+      </main>
+    </>
+  );
+}
